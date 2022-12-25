e7ca626e3259f643df67f8fd42f30b9bd6fe1031
minjun7410 <minjun7410@gmail.com>
2022-12-25 18:58:59 +0900

    Initial Commit

---
+++ FutureDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FutureDiary
+//
+//  Created by 송민준 on 2022/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
