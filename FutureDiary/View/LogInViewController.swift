a8fbc72b43509cfcd92d048330e709723cc5c1b9
minjun7410 <minjun7410@gmail.com>
2023-01-12 00:17:07 +0900

    Add LogIn function without closure for modify text field

---
+++ FutureDiary/View/LogInViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LogInViewController.swift
+//  FutureDiary
+//
+//  Created by 송민준 on 2023/01/11.
+//
+
+import UIKit
+
+class LogInViewController : UIViewController {
+    
+    @IBOutlet weak var emailTextBar: UITextField!
+    @IBOutlet weak var passwordTextBar: UITextField!
+    @IBOutlet weak var statusBar: UILabel!
+    
+    let authViewModel = AuthViewModel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func logInButtonClicked(_ sender: Any) {
+        let email:String = emailTextBar.text!
+        let password:String = passwordTextBar.text!
+        if authViewModel.logIn(email:email, password:password){
+            statusBar.text = "로그인 성공"
+        }
+        else{
+            statusBar.text = "이메일 또는 비밀번호가 옳지 않습니다."
+        }
+    }
+}
---
+++ FutureDiary/ViewModel/AuthViewModel.swift
@@ -0,0 +1,33 @@
+//
+//  AuthViewModel.swift
+//  FutureDiary
+//
+//  Created by 송민준 on 2023/01/11.
+//
+
+import Foundation
+import FirebaseAuth
+class AuthViewModel : NSObject {
+    var currentUser: String?
+    func checkLogIned(user:String) -> Bool{
+        let curUser = currentUser ?? "Nothing"
+        if curUser == user{
+            return true
+        }
+        else {return false}
+    }
+    func logIn(email:String, password:String) -> Bool {
+        print("Try Login: \(email), \(password)")
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            if user != nil {
+                self.currentUser = user?.description
+                print("Log In Success!")
+            }
+            else{
+                print("Log In Failed. \(error.debugDescription)")
+            }
+        }
+        
+        return true
+    }
+}
